de4502f588e1fc7e5e27611c88e1e9d28dada747
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-06-16 11:17:37 -0400

    fixed inquirer, logo is generating the code but not rendering an image

--- index.js
+++ index.js
@@ -1,66 +1,123 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const { Circle, Square, Triangle } = require("./lib/shapes");
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "text",
-    message: "Enter the text to appear on your logo, up to 3 characters",
-    validate: function (userInput) {
-      if (userInput.length > 3) {
-        return "logo text should be three characters or less";
-      }
-      return true;
-    },
+
+inquirer.prompt ([
+
+    {
+    type: 'input',
+    name: 'text',
+    message: 'Enter the text to appear on your logo, up to 3 characters',
   },
   {
-    type: "input",
-    name: "textColor",
-    message: "Enter either the name or hex code for the text color",
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter either the name or hex code for the text color',
   },
   {
-    type: "list",
-    name: "shape",
-    message: "Choose a shape from the list below",
-    choices: ["Circle", "Triangle", "Square"],
+    type: 'list',
+    name: 'shape',
+    message: 'Choose a shape from the list below',
+    choices: ['Circle', 'Triangle', 'Square'],
   },
   {
-    type: "input",
-    name: "shapeColor",
-    message: "Enter either the name or the hex code for the shape color",
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter either the name or the hex code for the shape color',
   },
-]);
 
-function generateSVG(data) {
-  let shape;
+])
 
-  if (data.shape === "Circle") {
-    shape = new Circle(data);
-  } else if (data.shape === "Triangle") {
-    shape = new Triangle(data);
-  } else {
-    shape = new Square(data);
-  }
+.then((input) => {
+     let userTextInput
 
-  return shape.render();
-}
+        if (input.text.length > 0 && input.text.length < 4) {
+            userTextInput = input.text
+        } else {
+            console.log('Invalid input, please enter 1-3 characters')
+            return
+        }
 
-fs.writeFile("logo.svg", generateSVG(data), (err) =>
-  err ? console.log(err) : console.log("SVG logo generated!")
-);
+    let shape;
 
-function init() {
-  inquirer
-    .prompt(prompts)
-    .then((data) => {
-      const logo = generateSVG(data);
-      writeToFile("logo.svg", logo);
-    })
-    .catch((err) => console.error(err));
-}
+    if (input.shape === 'Circle') {
+        shape = new Circle()
+    }
+    else if (input.shape === 'Square') {
+        shape = new Square()
+    }
+    else if (input.shape === 'Triangle') {
+        shape = new Triangle()
+    }
 
-init();
+    shape.setColor(input.shapeColor);
+    shape.setText(input.text);
+    shape.setTextColor(input.textColor);
+
+   fs.writeFile('logo.svg', shape.render(), (err) =>
+    err ? console.log(err) : console.log('SVG logo generated!'))
+})
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+// function generateSVG(data) {
+    
+//     let userTextInput
+//     let shape
+
+//     if (input.text.length > 0 && input.text.length < 4) {
+//         userTextInput = input.text
+//     } else {
+//         console.log('Invalid input, please enter 1-3 characters')
+//         return
+//     }
+
+//     if (input.shape === 'Circle') {
+//         shape = new Circle()
+//     }
+//     else if (input.shape === 'Square') {
+//         shape = new Square()
+//     }
+//     else if (input.shape === 'Triangle') {
+//         shape = new Triangle()
+//     }
+
+//     shape.setColor(input.shapeColor);
+//     shape.setText(input.text);
+//     shape.setTextColor(input.textColor);
+
+// }
+  
+//   function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) =>
+//       err ? console.error(err) : console.log('SVG logo generated!')
+//     );
+//   }
+
+// function init() {
+//   inquirer
+//     .prompt(prompts)
+//     .then((data) => {
+//       const logo = generateSVG(data);
+//       writeToFile('logo.svg', logo);
+//     })
+//     .catch((err) => console.error(err));
+// }
+
+// init();
 
 // .then((input) => {
 //      var userTextInput = '';
