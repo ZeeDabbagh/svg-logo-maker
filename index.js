1784d203d2bc365d4337e13ce85ba88394e1e770
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-06-16 10:49:19 -0400

    fixed function to generate logo but now inquirer is quitting on me

--- index.js
+++ index.js
@@ -1,57 +1,93 @@
-const fs = require('fs')
-const inquirer = require('inquirer')
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Square, Triangle } = require("./lib/shapes");
 
+inquirer.prompt([
+  {
+    type: "input",
+    name: "text",
+    message: "Enter the text to appear on your logo, up to 3 characters",
+    validate: function (userInput) {
+      if (userInput.length > 3) {
+        return "logo text should be three characters or less";
+      }
+      return true;
+    },
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter either the name or hex code for the text color",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Choose a shape from the list below",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter either the name or the hex code for the shape color",
+  },
+]);
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter the text to appear on your logo, up to 3 characters'
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter either the name or hex code for the text color'
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a shape from the list below',
-            choices: ['Circle', 'Triangle', 'Square']
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter either the name or the hex code for the shape color'
-        }
-    ])
-    .then((input) => {
-        const userTextInput = '';
+function generateSVG(data) {
+  let shape;
 
-            if (input.text.length > 0 && input.text.length < 4) {
-                userTextInput = input.text
-            } else {
-                console.log('Invalid input, please enter 1-3 characters')
-                return
-            }
+  if (data.shape === "Circle") {
+    shape = new Circle(data);
+  } else if (data.shape === "Triangle") {
+    shape = new Triangle(data);
+  } else {
+    shape = new Square(data);
+  }
 
-        let shape;
+  return shape.render();
+}
 
-        if (input.shape === 'Circle') {
-            shape = new Circle
-        } 
-        else if (input.shape === 'Square') {
-            shape = new Square
-        }
-        else if (input.shape === 'Triangle') {
-            shape = new Triangle
-        }
+fs.writeFile("logo.svg", generateSVG(data), (err) =>
+  err ? console.log(err) : console.log("SVG logo generated!")
+);
 
-        
-
-
-       fs.writeFile('logo.svg', (err) => 
-        err ? console.log(err) : console.log('SVG logo generated!')) 
+function init() {
+  inquirer
+    .prompt(prompts)
+    .then((data) => {
+      const logo = generateSVG(data);
+      writeToFile("logo.svg", logo);
     })
-    
+    .catch((err) => console.error(err));
+}
+
+init();
+
+// .then((input) => {
+//      var userTextInput = '';
+
+//         if (input.text.length > 0 && input.text.length < 4) {
+//             userTextInput = input.text
+//         } else {
+//             console.log('Invalid input, please enter 1-3 characters')
+//             return
+//         }
+
+//     var shape;
+
+//     if (input.shape === 'Circle') {
+//         shape = new Circle()
+//     }
+//     else if (input.shape === 'Square') {
+//         shape = new Square()
+//     }
+//     else if (input.shape === 'Triangle') {
+//         shape = new Triangle()
+//     }
+
+//     shape.setColor(input.shapeColor);
+//     shape.setText(input.text);
+//     shape.setTextColor(input.textColor);
+
+//    fs.writeFile('logo.svg', shape.render(), (err) =>
+//     err ? console.log(err) : console.log('SVG logo generated!'))
+// })
